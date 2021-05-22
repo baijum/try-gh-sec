9b70e262216fe408676cfbb9e93c708a6a21a603
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-05-22 21:04:16 +0530

    cp

--- sec.py
+++ sec.py
@@ -2,19 +2,20 @@ import os
 import base64
 
 def main():
-    repo = os.environ.get("REPO")
-    print(f"Hello! {repo}", repo)
-    repob64 = base64.b64encode(bytes(repo, "utf-8"))
-    print(f"Hello! {repob64}", repob64)
-    repob64d = base64.b64decode(repob64)
-    print(f"Hello! {repob64d}", repob64d)
-    print("-----------")
-    repo2 = os.environ.get("REPO2")
-    repo2b64d = base64.b64decode(repo2)
-    print(f"Hello! {repo2b64d}", repo2b64d)
-    print("-----------")
+    #repo = os.environ.get("REPO")
+    #print(f"Hello! {repo}", repo)
+    #repob64 = base64.b64encode(bytes(repo, "utf-8"))
+    #print(f"Hello! {repob64}", repob64)
+    #repob64d = base64.b64decode(repob64)
+    #print(f"Hello! {repob64d}", repob64d)
+    #print("-----------")
+    #repo2 = os.environ.get("REPO2")
+    #repo2b64d = base64.b64decode(repo2)
+    #print(f"Hello! {repo2b64d}", repo2b64d)
+    #print("-----------")
+
     repo3 = os.environ.get("REPO3")
-    repo3b64d = base64.b64decode(repo3[1:])
+    repo3b64d = base64.b64decode(repo3)
     print(f"Hello! {repo3b64d}", repo3b64d)
 
 if __name__ == "__main__":
