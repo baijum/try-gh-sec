aaea9d521df189ccac1358fb52adb937f85e0ba8
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-05-22 20:50:44 +0530

    cp

--- sec.py
+++ sec.py
@@ -8,6 +8,10 @@ def main():
     print(f"Hello! {repob64}", repob64)
     repob64d = base64.b64decode(repob64)
     print(f"Hello! {repob64d}", repob64d)
+    print("-----------")
+    repo2 = os.environ.get("REPO2")
+    repo2b64d = base64.b64decode(repo2)
+    print(f"Hello! {repo2b64d}", repo2b64d)
 
 if __name__ == "__main__":
     main()
